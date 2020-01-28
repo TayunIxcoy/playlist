42569ec481f8cfdf2c171a52f92f5c91f9bdd618
TayunIxcoy <yonathantayun12@gmail.com>
2020-01-28 02:27:15 +0000

    almost

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let SongList = ['Soy el diablo', 'Radicamos en South central', 'Con calma']
-let ArtistList = ['Bad Buny', 'Fuerza Regida', 'Daddy Yanky']
+/*let SongList = ['Soy el diablo', 'Radicamos en South central', 'Con calma']
+/*let ArtistList = ['Bad Buny', 'Fuerza Regida', 'Daddy Yanky']
 let Image = []
 let length=[]
 let link=[]
 
 
 
-SongList.forEach(function(song) {
+//SongList.forEach(function(song) {
         $(`.songName`).append(`<p>${song}</p> `)
 });
 
@@ -25,4 +25,32 @@ length.forEach(function(link){
  $('.addButton').click(function() {
 
  });
-  
+  */
+ var songOne={ 
+     Artist:"Bud Bunny",
+     name:"soy el diablo",
+     Image:"",
+     Link:"",
+    length:""
+    }
+     var songTwo={ 
+     Artist:"Daddy Yankee",
+     name:"Con Calma",
+     Image:"https://m.media-amazon.com/images/I/A1GNxnRnUbL._SS500_.jpg",
+     Link:"",
+    length:""
+    }
+     var songThree={ 
+     Artist:"Radicamos En South Central",
+     name:"Fuerza Regida",
+     Image:"",
+     Link:"",
+    length:""
+    }
+    var Music = [songOne, songTwo, songThree, ]
+    
+    //music.forEach(function(music) {
+    //$('.music').append()
+//} );
+ $("songOne").append("<p> Artist : "+songOne["Artist"]+"</p>");
+ $("songTwo").append("<p> Artist : "+info[""]+"</p>");
